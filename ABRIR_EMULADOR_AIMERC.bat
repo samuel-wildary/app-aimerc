133ee35c21114938154febe786eb4950fe271261
Samuel Wildary <samuel.wildary@local>
2026-07-22 10:34:33 -0300

    fix: remove parametro -scale do emulador para corrigir travamento de clique/touch

--- ABRIR_EMULADOR_AIMERC.bat
+++ ABRIR_EMULADOR_AIMERC.bat
@@ -1,7 +1,7 @@
 @echo off
+title AiMerc - Android Emulator
 set EMULATOR=C:\Users\Samuel Wildary\AppData\Local\Android\Sdk\emulator\emulator.exe
 set AVD=Pixel_10_Pro
-set SCALE=0.55
 
-echo Abrindo Android Emulator %AVD% em escala %SCALE%...
-"%EMULATOR%" -avd %AVD% -scale %SCALE%
+echo Abrindo Android Emulator %AVD%...
+"%EMULATOR%" -avd %AVD%
--- INICIAR_EMULADOR.bat
+++ INICIAR_EMULADOR.bat
@@ -2,9 +2,8 @@
 title AiMerc - Android Emulator
 set EMULATOR=C:\Users\Samuel Wildary\AppData\Local\Android\Sdk\emulator\emulator.exe
 set AVD=Pixel_10_Pro
-set SCALE=0.55
 
 echo ===================================================
 echo Iniciando Android Emulator (%AVD%)...
 echo ===================================================
-"%EMULATOR%" -avd %AVD% -scale %SCALE%
+"%EMULATOR%" -avd %AVD%
